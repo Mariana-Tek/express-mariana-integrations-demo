4ab898c3728c7e70db05a2c21fe83cf3948d3f0d
Welch Canavan <welch.canavan@gmail.com>
2017-11-01 16:27:07 -0400

    add locations view, route

--- routes/demo/index.js
+++ routes/demo/index.js
@@ -9,6 +9,7 @@ router.get('/', (req, res) => {
 router.use('/account', require('./account'));
 router.use('/login', require('./login'));
 router.use('/location', require('./location'));
+router.use('/locations', require('./locations'));
 router.use('/register', require('./register'));
 router.use('/reset', require('./reset'));
 router.use('/store', require('./store'));
