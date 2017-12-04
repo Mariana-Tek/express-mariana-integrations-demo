bd4e09d0d9752ef2c6056ef2cb9da720861a4110
Welch Canavan <welch.canavan@gmail.com>
2017-12-04 16:01:37 -0500

    add tenant brand image requests

--- middleware/make-requests.js
+++ middleware/make-requests.js
@@ -1,9 +1,8 @@
-const reqProm = require('request-promise');
+const camelCase = require('lodash.camelcase');
+const reqProm = require('request-promise-native');
 const sortBy = require('lodash.sortby');
 
 module.exports = function (req, res, next) {
-    // https://cousteau-r45kxk.marianatek.com/api/tenants
-
     const reqOptions = (path) => {
         return {
             uri: `https://${res.locals.endpoint}.marianatek.com/api/${path}`,
@@ -14,17 +13,19 @@ module.exports = function (req, res, next) {
         };
     };
 
-    const reqTenant = reqProm(reqOptions('tenants'))
-        .then(response => {
-            res.locals.siteTitle = response.data[0].attributes.name;
-        })
-        .catch(error => {
-            console.error(error);
-        });
+    const reqImage = (id, relationship) => {
+        return reqProm(reqOptions(`public_images/${id}`))
+            .then(response => {
+                res.locals[relationship] = response.data.attributes.image;
+            })
+            .catch(error => {
+                console.error(error);
+            });
+    };
 
     const reqLocations = reqProm(reqOptions('locations?page_size=100'))
         .then(response => {
-            const locationsInfo = response.data.map((location) => {
+            const locationsInfo = response.data.map(location => {
                 return {
                     locationId: location.id,
                     name: location.attributes.name,
@@ -38,7 +39,35 @@ module.exports = function (req, res, next) {
             console.error(error);
         });
 
-    Promise.all([reqTenant, reqLocations]).then(() => {
-        next();
-    });
+    const reqTenant = reqProm(reqOptions('tenants'))
+        .then(response => {
+            res.locals.siteTitle = response.data[0].attributes.name;
+        })
+        .catch(error => {
+            console.error(error);
+        });
+
+    const reqTenantBrand = reqProm(reqOptions('tenant_brands'))
+        .then(response => {
+            return response.data[0].relationships;
+        })
+        .catch(error => {
+            console.error(error);
+        });
+
+    Promise.all([reqTenantBrand, reqTenant, reqLocations])
+        .then((results) => {
+            // after tenant brand information is returned retrieve tenant brand assets
+
+            const relationships = results[0];
+
+            const relationshipPromises = Object.keys(relationships).map(relationship => {
+                return reqImage(relationships[relationship].data.id, camelCase(relationship));
+            });
+
+            return Promise.all(relationshipPromises);
+        })
+        .then(() => {
+            next();
+        });
 };
