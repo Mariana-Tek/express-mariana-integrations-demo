a6f3f2fadb5709f35e2f884792b4c00fde844b0a
Welch Canavan <welch.canavan@gmail.com>
2017-12-20 14:13:59 -0500

    add style toggles

---
+++ public/javascripts/.eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+    "env": {
+        "browser": true
+    },
+    "rules": {
+        // way easier to reset this stuff and use vanilla ES5 than incorporate babel
+        // anyone is welcome to make the switch if they feel it is important
+        "no-var": 0,
+        "prefer-arrow-callback": 0
+    }
+};
