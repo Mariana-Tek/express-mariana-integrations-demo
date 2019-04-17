440e032894ebd7d68af901b93a9667f847f6aaee
Aaron Wentt <aaron.w@lookthink.com>
2019-04-16 21:45:34 -0400

    adjusting values so we check for environment vars where necessary

--- middleware/set-locals.js
+++ middleware/set-locals.js
@@ -1,6 +1,13 @@
+const setEndpoint = endpointArg => {
+    const marianaEndpoint = process.env.MARIANA_ENDPOINT;
+    const marianaEndpointExists = !!marianaEndpoint;
+
+    return marianaEndpointExists ? marianaEndpoint : (endpointArg || 'mtdemostable-is7hfd');
+};
+
 module.exports = function (req, res, next) {
     res.locals.baseUrl = req.baseUrl;
-    res.locals.endpoint = req.params.endpoint ? req.params.endpoint : 'mtdemostable-is7hfd';
+    res.locals.endpoint = setEndpoint(req.params.endpoint);
 
     if (req.params.versionOrBranch) {
         if (req.params.versionOrBranch.split('.').length === 3) {
